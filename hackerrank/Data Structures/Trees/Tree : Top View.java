bf34892b41ce0212851e00650de67d5e604ac00a
ForgedForgery <hiandreas@googlemail.com>
2017-11-19 13:39:36 +0100

    Create Tree : Top View.java

---
+++ hackerrank/Data Structures/Trees/Tree : Top View.java
@@ -0,0 +1,23 @@
+   /* 
+    
+    class Node 
+       int data;
+       Node left;
+       Node right;
+   */
+   void topView(Node root) {
+      leftView(root);
+      rightView(root.right);
+    }
+    void leftView(Node root) {
+        if(root == null)
+            return;
+        leftView(root.left);
+        System.out.print(root.data + " ");
+    }
+    void rightView(Node root) {
+        if(root == null)
+            return;
+        System.out.print(root.data + " ");
+        rightView(root.right);
+    }
