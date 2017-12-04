28417b059e5406b584e4dcd421986426df0a91be
ForgedForgery <hiandreas@googlemail.com>
2017-12-04 12:55:11 +0100

    Update Tree: Level Order Traversal.java

--- hackerrank/Data Structures/Trees/Tree: Level Order Traversal.java
+++ hackerrank/Data Structures/Trees/Tree: Level Order Traversal.java
@@ -1,4 +1,19 @@
-   /* 
+	static void levelOrderWithQueue(Node root){
+  		Queue<Node> queue = new Queue<Node>();
+        queue.Enqueue(root);
+        
+        while(queue.Count != 0) {
+            Node current = queue.Dequeue();
+            Console.Write(current.data + " ");
+            if(current.left != null)
+                queue.Enqueue(current.left);
+            if(current.right != null)
+                queue.Enqueue(current.right);
+        }
+        
+    }
+
+/* 
     
     class Node 
        int data;
